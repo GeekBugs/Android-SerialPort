81c2b39194a6c894ce87b3fdbfdb4e9eb38d268b
F1ReKing <284061805@qq.com>
2019-10-31 14:11:00 +0800

    Initial commit

---
+++ app/src/main/java/com/mengbo/android_serialport/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mengbo.android_serialport;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
